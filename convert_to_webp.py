93ef347f8a99a30ec3c4f041b810718f510d4a3e
Jelly <Jellybob2016@outlook.com>
2023-11-08 06:25:40 +0000

    Add moving old png files

--- convert_to_webp.py
+++ convert_to_webp.py
@@ -18,12 +18,13 @@
 
 import os  # For OS level commands
 import pathlib
+import shutil
 import subprocess
 import time
 
-def get_images(working_dir=os.curdir, extensions=('.png', '.jpg', '.jpeg')):
+
+def get_images(directory=pathlib.Path(os.curdir), extensions=('.png', '.jpg', '.jpeg')):
     try:
-        directory = pathlib.Path(working_dir)
         images = directory.rglob('*')
         return [_ for _ in images if _.suffix.lower() in extensions]
 
@@ -31,7 +32,7 @@ def get_images(working_dir=os.curdir, extensions=('.png', '.jpg', '.jpeg')):
         pass
 
 
-def convert_to_webp(file: pathlib.Path, quality: int = 80) -> bool:
+def convert_to_webp(root_path: pathlib.Path, file: pathlib.Path, quality: int = 80) -> bool:
     command = f'cwebp -q {quality} "{file.absolute()}" -o "{file.parent}\\{file.stem}.webp"'
     try:
         process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -41,6 +42,14 @@ def convert_to_webp(file: pathlib.Path, quality: int = 80) -> bool:
         pass
 
     if pathlib.WindowsPath(f'{file.parent}\\{file.stem}.webp').exists():
+
+        relative_path = file.relative_to(root_path)
+
+        if not pathlib.Path(f'{root_path.parent}\\pngs\\{relative_path.parent}').exists():
+            os.mkdir(f'{root_path.parent}\\pngs\\{relative_path.parent}')
+
+        print(f'{root_path.parent}\\pngs\\{relative_path.parent}')
+        shutil.move(file.absolute(), f'{root_path.parent}\\pngs\\{relative_path}')
         return True
 
     return False
@@ -60,10 +69,12 @@ dir_name = r'C:\Users\jelly\Documents\Programming\Python\batch-conversion-to-web
 default_extensions = ('.png', '.jpg', '.jpeg')  # Image Extensions
 global_quality = 100  # Image quality
 
-list_of_images = get_images(dir_name, default_extensions)
+dir_path = pathlib.Path(dir_name)
+
+list_of_images = get_images(dir_path, default_extensions)
 
 for image in list_of_images:
-    result = convert_to_webp(image, global_quality)
+    result = convert_to_webp(dir_path, image, global_quality)
     if result:
         print(f'{image.name} converted successfully.')
     else:
